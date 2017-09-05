7bcfedfbc377358ee00dcbd5b6d458dcb07b6d4d
mjmersenski <mjmersenski@10.30.16.69>
2017-09-05 18:09:14 -0500

    removed all the system.out.println calls and used class encapsualtion

--- src/lab3/Employee.java
+++ src/lab3/Employee.java
@@ -103,7 +103,11 @@ public class Employee {
     // doFirtTimeOrientation()
     private void meetDepartmentStaff() {
         metDeptStaff = true;
+        /*
         System.out.println(firstName + " " + lastName + " met with Dept. Staff on "
+            + getFormattedDate());
+        */
+        output.simpleOutput(firstName + " " + lastName + " met with Dept. Staff on "
             + getFormattedDate());
     }
 
@@ -112,7 +116,11 @@ public class Employee {
     // independently from other classes.
     public void reviewDeptPolicies() {
         reviewedDeptPolicies = true;
+        /*
         System.out.println(firstName + " " + lastName + " reviewed Dept policies on "
+            + getFormattedDate());
+        */
+        output.simpleOutput(firstName + " " + lastName + " reviewed Dept policies on "
             + getFormattedDate());
     }
 
@@ -122,7 +130,11 @@ public class Employee {
     public void moveIntoCubicle(String cubeId) {
         this.cubeId = cubeId;
         this.movedIn = true;
+        /*
         System.out.println(firstName + " " + lastName + " moved into cubicle "
+                + cubeId + " on " + getFormattedDate());
+        */
+        output.simpleOutput(firstName + " " + lastName + " moved into cubicle "
                 + cubeId + " on " + getFormattedDate());
     }
 
